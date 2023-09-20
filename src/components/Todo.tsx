baffcae5a26243d19fa18ef2d0261e5abea35a06
ss5815190 <ss5815190@gmail.com>
2023-09-20 09:00:00 +0800

    解構

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../store/store';
+import { RootState,AppDispatch } from '../store/store';
 import { todoListAction } from '../store/todoSlice';
 import TodoItem from './TodoItem';
 
 function Todo() {
-    const todoList = useSelector((state:RootState)=>state.todoListSlice);
-    const dispatch =useDispatch();
+    const {todos} = useSelector((state:RootState)=>state.todoListSlice);
+    const dispatch:AppDispatch =useDispatch();
     const removeHandler =(id:string)=>{
         dispatch(todoListAction.deleteTodo(id));
     }
   return (
     <ul className="w-8/12 max-w-2xl">
-      {todoList.todos.map((item)=>(
+      {todos.map((item)=>(
         <TodoItem key={item.id} text={item.text} onRemoveTodo={()=>removeHandler(item.id)} />
       ))}
     </ul>
